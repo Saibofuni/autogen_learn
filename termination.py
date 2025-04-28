52551f1331c6259cf9a26f884fdffedc645ae648
tiancaiwwzzyy <ziyiwang236@outlook.com>
2025-04-28 13:27:34 +0800

    完善了autogen0.2现有的三种终结对话方式

--- termination.py
+++ termination.py
@@ -11,6 +11,7 @@ config_list = {
     "api_version": "2024-12-01-preview",
 }
 
+# use message to end the conversation
 user_proxy = ConversableAgent(
     name="User_Proxy",
     system_message="You are a user proxy. You will ask the agents to do some math operations.",
@@ -26,7 +27,43 @@ assistant = ConversableAgent(
     human_input_mode="NEVER",
 )
 
+print("use message to end the conversation")
+
 chat_message = user_proxy.initiate_chat(
     assistant,
     message="Please add 1 to 2 and return the result.",
 )
+#---------------------------------------------------------------------
+
+# set the max turn to end the conversation
+
+print("set the max turn to end the conversation")
+chat_message = user_proxy.initiate_chat(
+    assistant,
+    message="Please add 1 to 2 and return the result.",
+    max_turn=3,
+)
+#---------------------------------------------------------------------
+
+# set the max_consecutive_auto_reply inside the agent
+user_proxy2 = ConversableAgent(
+    name="User_Proxy",
+    system_message="You are a user proxy. You will ask the agents to do some math operations.",
+    llm_config={"config_list": config_list},
+    human_input_mode="NEVER",
+    max_consecutive_auto_reply=1,
+)
+
+assistant2 = ConversableAgent(
+    name="Assistant",
+    system_message="You are an assistant. You will do some math operations. If the tast is done, you need to return 'TERMINATION' to the user proxy.",
+    llm_config={"config_list": config_list},
+    human_input_mode="NEVER",
+)
+
+print("set the max_consecutive_auto_reply inside the agent")
+
+chat_message = user_proxy2.initiate_chat(
+    assistant2,
+    message="Please add 1 to 2 and return the result.",
+)#---------------------------------------------------------------------
