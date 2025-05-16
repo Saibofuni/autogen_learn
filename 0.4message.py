2c48445ad341287040870a323e30d5b1175c0ce5
tiancaiwwzzyy <ziyiwang236@outlook.com>
2025-05-16 14:45:02 +0800

    autogen0.4 messages

---
+++ 0.4message.py
@@ -0,0 +1,16 @@
+# normal message
+from autogen_agentchat.messages import TextMessage
+text_message = TextMessage(content="Hello!", source="User")
+
+# multimodal message
+from io import BytesIO
+
+import requests
+from autogen_agentchat.messages import MultiModalMessage
+from autogen_core import Image as AGImage
+from PIL import Image
+
+pil_image = Image.open(BytesIO(requests.get("https://picsum.photos/300/200").content))
+img = AGImage(pil_image)
+multi_modal_message = MultiModalMessage(content=["Can you describe the content of this image?", img], source="User")
+img
